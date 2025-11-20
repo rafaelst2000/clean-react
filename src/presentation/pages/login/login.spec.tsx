00a0f8ed06b57c236fa655e1e8ba0cd2bb6aacc9
Rafael Trevisan <rafaelst2000@gmail.com>
2025-11-20 13:41:47 -0300

    test: ensure login present spinner on submit

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -116,4 +116,23 @@ describe('Login Component', () => {
     const submitButton = getByTestId('submit') as HTMLButtonElement
     expect(submitButton.disabled).toBe(false)
   })
+
+  test('Should show spinner on submit', () => {
+    const { sut } = makeSut()
+    const { getByTestId } = sut
+
+    const email = faker.internet.email()
+    const emailInput = getByTestId('email')
+    fireEvent.input(emailInput, { target: { value: email } })
+
+    const password = faker.internet.password()
+    const passwordInput = getByTestId('email')
+    fireEvent.input(passwordInput, { target: { value: password } })
+
+    const submitButton = getByTestId('submit') as HTMLButtonElement
+    fireEvent.click(submitButton)
+    
+    const spinner = getByTestId('spinner')
+    expect(spinner).toBeTruthy()
+  })
 })
