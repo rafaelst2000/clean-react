b1c4d829f456dcf7a13693998094fce2919c8fb9
Rafael Trevisan <rafaelst2000@gmail.com>
2025-11-23 14:08:40 -0300

    refactor: rename SetStorageMock

--- src/data/usecases/save-access-token/local-save-access-token.spec.ts
+++ src/data/usecases/save-access-token/local-save-access-token.spec.ts
@@ -1,19 +1,28 @@
 import { LocalSaveAccessToken } from './local-save-access-token'
-import { SetStorageSpy } from '@/data/test'
+import { SetStorageMock } from '@/data/test'
 import faker from 'faker'
 
 type SutTypes = {
   sut: LocalSaveAccessToken
-  setStorageSpy: SetStorageSpy
+  setStorageSpy: SetStorageMock
 }
 
 const makeSut = (): SutTypes => {
-  const setStorageSpy = new SetStorageSpy()
+  const setStorageSpy = new SetStorageMock()
   const sut = new LocalSaveAccessToken(setStorageSpy)
   return { sut, setStorageSpy }
 }
 
 describe('LocalSaveAccessToken', () => {
+  test('Should cal SetStorage with correct value', async () => {
+    const { sut, setStorageSpy } = makeSut()
+    const accessToken = faker.datatype.uuid()
+    await sut.save(accessToken)
+
+    expect(setStorageSpy.key).toBe('accessToken')
+    expect(setStorageSpy.value).toBe(accessToken)
+  })
+
   test('Should cal SetStorage with correct value', async () => {
     const { sut, setStorageSpy } = makeSut()
     const accessToken = faker.datatype.uuid()
