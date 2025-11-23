0b6ef27a5451a83a852b3ec0aec076a0dbef4545
Rafael Trevisan <rafaelst2000@gmail.com>
2025-11-23 15:23:28 -0300

    chore: release 1.3.0

--- src/main/factories/pages/login/login-factory.tsx
+++ src/main/factories/pages/login/login-factory.tsx
@@ -2,11 +2,13 @@ import React from 'react'
 import { Login } from '@/presentation/pages'
 import { makeRemoteAuthentication } from '@/main/factories/usecases/authentication/remote-authenticaton-factory'
 import { makeLoginValidation } from './login-validation-factory'
+import { makeLocalSaveAccessToken } from '../../usecases/save-access-token/local-save-access-token-factory'
 
 export const makeLogin: React.FC = () => {
   return (
     <Login
       authentication={makeRemoteAuthentication()}
+      saveAccessToken={makeLocalSaveAccessToken()}
       validation={makeLoginValidation()}
     />
   )
