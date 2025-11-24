7b59ab77cf97a048240b688428c9adc3445952ae
Rafael Trevisan <rafaelst2000@gmail.com>
2025-11-24 20:41:34 -0300

    test: ensure RemoteAddAccount throws UnexpectedError

--- src/data/usecases/add-account/remote-add-account.spec.ts
+++ src/data/usecases/add-account/remote-add-account.spec.ts
@@ -5,7 +5,7 @@ import { AddAccountParams } from '@/domain/usecases'
 import { AccountModel } from '@/domain/models'
 import { mockAddAccount } from '@/domain/test'
 import faker from 'faker'
-import { EmailInUseError } from '@/domain/error'
+import { EmailInUseError, UnexpectedError } from '@/domain/error'
 
 type SutTypes = {
   sut: RemoteAddAccount
@@ -44,5 +44,15 @@ describe('RemoteAddAccount', () => {
     const addAccountParams = mockAddAccount()
     const promise = sut.add(addAccountParams)
     await expect(promise).rejects.toThrow(new EmailInUseError())
+  })
+
+  test('Should throw UnexpectedError if HttpPostClient returns 400', async () => {
+    const { sut, httpPostClientSpy } = makeSut()
+    httpPostClientSpy.response = {
+      statusCode: HttpStatusCode.badRequest
+    }
+    const addAccountParams = mockAddAccount()
+    const promise = sut.add(addAccountParams)
+    await expect(promise).rejects.toThrow(new UnexpectedError())
   })
 })
