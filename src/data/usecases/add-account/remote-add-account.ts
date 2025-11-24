691dae886ef33fada93c7de2025d14e84fdc8c08
Rafael Trevisan <rafaelst2000@gmail.com>
2025-11-24 20:41:24 -0300

    feat: ensure RemoteAddAccount throws UnexpectedError

--- src/data/usecases/add-account/remote-add-account.ts
+++ src/data/usecases/add-account/remote-add-account.ts
@@ -1,5 +1,5 @@
 import { HttpPostClient, HttpStatusCode } from '@/data/protocols/http'
-import { EmailInUseError } from '@/domain/error'
+import { EmailInUseError, UnexpectedError } from '@/domain/error'
 import { AccountModel } from '@/domain/models'
 import { AddAccount, AddAccountParams } from '@/domain/usecases'
 
@@ -15,7 +15,7 @@ export class RemoteAddAccount implements AddAccount {
     switch (httpResponse.statusCode) {
       case HttpStatusCode.ok: return httpResponse.body
       case HttpStatusCode.forbidden: throw new EmailInUseError()
-      default: return null
+      default: throw new UnexpectedError()
     }
   }
 }
