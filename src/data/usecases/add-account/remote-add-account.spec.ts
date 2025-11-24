9d5dfddfb2c4add5c472aba0d5c0eddb8088e726
Rafael Trevisan <rafaelst2000@gmail.com>
2025-11-24 20:43:19 -0300

    test: ensure RemoteAddAccount throws UnexpectedError if returns 404

--- src/data/usecases/add-account/remote-add-account.spec.ts
+++ src/data/usecases/add-account/remote-add-account.spec.ts
@@ -65,4 +65,14 @@ describe('RemoteAddAccount', () => {
     const promise = sut.add(addAccountParams)
     await expect(promise).rejects.toThrow(new UnexpectedError())
   })
+
+  test('Should throw UnexpectedError if HttpPostClient returns 404', async () => {
+    const { sut, httpPostClientSpy } = makeSut()
+    httpPostClientSpy.response = {
+      statusCode: HttpStatusCode.notFound
+    }
+    const addAccountParams = mockAddAccount()
+    const promise = sut.add(addAccountParams)
+    await expect(promise).rejects.toThrow(new UnexpectedError())
+  })
 })
