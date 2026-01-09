7cde2251410bdf8ef6576c19862828de4ae582d2
Rafael Trevisan <rafaelst2000@gmail.com>
2026-01-08 21:12:43 -0300

    test: prevent login to call submit multiple times

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -90,7 +90,32 @@ describe('Login', () => {
     cy.url().should('eq', baseUrl + '/login')
   })
 
-  it.only('Should present save accessToken if valid credentials are provided', () => {
+  // it.only('Should present save accessToken if valid credentials are provided', () => {
+  //   cy.intercept({
+  //     method: 'POST',
+  //     url: /login/
+  //   }, {
+  //     statusCode: 200,
+  //     response: {
+  //       accessToken: faker.datatype.uuid()
+  //     }
+  //   }).as('request')
+
+  //   cy.getByTestId('email').focus()
+  //   cy.getByTestId('email').type(faker.internet.email())
+
+  //   cy.getByTestId('password').focus()
+  //   cy.getByTestId('password').type(faker.random.alphaNumeric(5))
+
+  //   cy.getByTestId('submit').click()
+  //   cy.getByTestId('main-error').should('not.exist')
+  //   cy.getByTestId('spinner').should('not.exist')
+
+  //   cy.url().should('eq', baseUrl + '/')
+  //   cy.window().then(window => assert.isOk(window.localStorage.getItem('accessToken')))
+  // })
+
+  it('Should prevent multiple submits', () => {
     cy.intercept({
       method: 'POST',
       url: /login/
@@ -107,11 +132,8 @@ describe('Login', () => {
     cy.getByTestId('password').focus()
     cy.getByTestId('password').type(faker.random.alphaNumeric(5))
 
-    cy.getByTestId('submit').click()
-    cy.getByTestId('main-error').should('not.exist')
-    cy.getByTestId('spinner').should('not.exist')
+    cy.getByTestId('submit').dblclick()
 
-    cy.url().should('eq', baseUrl + '/')
-    cy.window().then(window => assert.isOk(window.localStorage.getItem('accessToken')))
+    cy.get('@request.all').should('have.length', 1)
   })
 })
