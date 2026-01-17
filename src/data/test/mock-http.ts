78232341fc35060450831c7eb6c99b25ec79fb79
Rafael Trevisan <rafaelst2000@gmail.com>
2026-01-17 10:58:23 -0300

    test: ensure RemoteLoadSurveyList throws UnexpectedError on 403

--- src/data/test/mock-http.ts
+++ src/data/test/mock-http.ts
@@ -19,9 +19,14 @@ export class HttpPostClientSpy<ResponseType> implements HttpPostClient<ResponseT
   }
 }
 
-export class HttpGetClientSpy implements HttpGetClient {
+export class HttpGetClientSpy<ResponseType> implements HttpGetClient<ResponseType> {
   url: string
-  async get (params: HttpGetParams): Promise<void> {
+  response: HttpResponse<ResponseType> = {
+    statusCode: HttpStatusCode.ok
+  }
+
+  async get (params: HttpGetParams): Promise<HttpResponse<ResponseType>> {
     this.url = params.url
+    return this.response
   }
 }
--- src/data/usecases/load-survey-list/remote-load-survey-list.ts
+++ src/data/usecases/load-survey-list/remote-load-survey-list.ts
@@ -1,9 +1,15 @@
-import { HttpGetClient } from '@/data/protocols/http'
+import { HttpGetClient, HttpStatusCode } from '@/data/protocols/http'
+import { UnexpectedError } from '@/domain/error'
 
 export class RemoteLoadSurveyList {
   constructor (private readonly url: string, private readonly httpGetClient: HttpGetClient) {}
 
   async loadAll (): Promise<void> {
-    await this.httpGetClient.get({ url: this.url })
+    const httpResponse = await this.httpGetClient.get({ url: this.url })
+
+    switch (httpResponse.statusCode) {
+      case HttpStatusCode.ok: break
+      default: throw new UnexpectedError()
+    }
   }
 }
